2b7939d4c94e975d8dc93dc625dc9407c840ebb1
MedAmine <med.amine.mzk@horizon-tech.tn>
2023-07-27 21:29:36 +0100

    navbar + bootstrap

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'my first controller : HomeController',
+        ]);
+    }
+
+    #[Route('/taher', name: 'app_taher')]
+    public function index1(): Response
+    {
+        return $this->render('home/taher.html.twig');
+    }
+}
